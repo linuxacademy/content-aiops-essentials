632672c9a5756a134c38c5381356a0aba48dfe12
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-03-13 12:20:22 -0400

    Create promql2.py

---
+++ promql2.py
@@ -0,0 +1,43 @@
+import requests
+
+# Prometheus api endpoint for query 
+URL = "http://localhost:9090/api/v1/query"
+
+# Memory query
+PROMQL1 = {'query':'(node_memory_MemTotal_bytes - (node_memory_MemFree_bytes + node_memory_Cached_bytes + node_memory_Buffers_bytes)) / node_memory_MemTotal_bytes * 100'}
+
+# CPU Query
+PROMQL2 = {'query':'100 - avg(irate(node_cpu_seconds_total{job="node",mode="idle"}[5m])) by (instance) * 100'}
+
+print("'row','instance','memory','cpu'")
+
+line_no = 1
+
+for seq in range(0 , 10):
+    rows = []
+    row = 0
+
+    r1 = requests.get(url = URL, params = PROMQL1)
+
+    r2 = requests.get(url = URL, params = PROMQL2)
+
+    r1_json = r1.json()['data']['result']
+    r2_json = r2.json()['data']['result']
+
+    for result in r1_json:
+        l = []
+        l.append(result['metric'].get('instance', ''))
+        l.append(result['value'][1])
+        rows.append(l)
+
+    for result in r2_json:
+        l = []
+        l.append(result['value'][1])
+        rows[row].append(l[0])
+        row = row + 1
+
+    for ro in rows:
+        line = str(line_no)
+        line = line + "," + ro[0] + "," + ro[1] + "," + ro[2]
+        print(str(line))
+        line_no = line_no + 1
