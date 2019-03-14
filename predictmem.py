9c5d16ee9adb1b3cd601e55f36263f18b3a7a5b9
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-03-14 16:56:12 -0400

    Update predictmem.py

--- predictmem.py
+++ predictmem.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import sys
 
 sys.__stdout__ = sys.stdout
