283466fe9a0e2aff9a6b6c4a38051d3f2ce1d15e
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-03-14 14:21:29 -0400

    Update predictmem.py

--- predictmem.py
+++ predictmem.py
@@ -1,5 +1,4 @@
 import sklearn
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import numpy as np
@@ -13,14 +12,11 @@ linear_regressor = LinearRegression()
 
 dataset = pd.read_csv('promql.csv')
 dataset2 = pd.read_csv('promql2.csv')
-#print(dataset.head())
-#print(dataset.describe)
 
 X = dataset[['replicas','cpu']]
 y = dataset['memory']
 
 X2 = dataset2[['replicas','cpu']]
-y2 = dataset2['memory']
 z2 = dataset2['replicas']
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.9,random_state=0)
@@ -31,4 +27,3 @@ pred_y = linear_regressor.predict(X2)
 
 df = pd.DataFrame({'Projected Replicas':z2,'Mem Increase (%)':pred_y})
 print(df)
-
