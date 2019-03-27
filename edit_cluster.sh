e5e05fd1268ae865bd57c6d09d9f80bf9a83ad1d
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-03-27 15:36:37 -0400

    Update edit_cluster.sh

--- edit_cluster.sh
+++ edit_cluster.sh
@@ -1,3 +1,5 @@
-cp cluster.yaml cluster.yaml.backup
-sed -i "s/      name: nodepool1/      name: nodepool1\n      count: $1/" cluster.yaml
-kube-aws apply
+cp instance.yaml instance.yaml.backup
+sed -i "s/  maxSize: 2/  maxSize: $1/" instance.yaml
+sed -i "s/  minSize: 2/  minSize: $1/" instance.yaml
+cat instance.yaml | kops replace -f -
+kops update cluster --yes
